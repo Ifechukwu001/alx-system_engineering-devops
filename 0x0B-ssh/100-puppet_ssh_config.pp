eb95d870c9852821c8da88d20da731a46b8a0e99
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2022-10-16 23:26:45 +0100

    puppet configs

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Makes changes to our configuration file
+
+# Set the config of a ssh_file
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'No Authenticate with passowrd':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
